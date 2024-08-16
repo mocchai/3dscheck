06e25cc1132d4011b769c528d8da267e3c737fbd
Lilia Aurora <oka@waffle.moe>
2024-08-16 17:21:42 -0500

    start work on... something

---
+++ include/images.hpp
@@ -0,0 +1,23 @@
+#include "common.hpp"
+
+typedef struct {
+    C2D_Sprite sprite,
+} Image;
+
+class ImageManager
+{
+private:
+    /* data */
+public:
+    ImageManager(/* args */);
+    ~ImageManager();
+};
+
+ImageManager::ImageManager(/* args */)
+{
+    
+}
+
+ImageManager::~ImageManager()
+{
+}
